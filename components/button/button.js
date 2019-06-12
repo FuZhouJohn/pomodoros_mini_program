03ce4a31defcef02b1e7fb774c7522bba2ffcf19
Zhuangjinan <a492073467@gmail.com>
2019-06-13 01:25:49 +0800

    button组件

---
+++ components/button/button.js
@@ -0,0 +1,24 @@
+Component({
+  properties: {
+    text: {
+      type: String,
+      value: ''
+    },
+    size: {
+      type: String,
+      value: ''
+    },
+    type: {
+      type: String,
+      value: 'default'
+    }
+  },
+  data: {
+
+  },
+  methods: {
+    onTap:function(){
+      this.triggerEvent('ztap')
+    }
+  }
+})
---
+++ pages/home/home.js
@@ -0,0 +1,37 @@
+// pages/home/home.js
+Page({
+
+  data: {
+
+  },
+
+  onLoad: function(options) {
+
+  },
+
+  onReady: function() {
+
+  },
+
+  onShow: function() {
+
+  },
+  onHide: function() {
+
+  },
+  onUnload: function() {
+
+  },
+  onPullDownRefresh: function() {
+
+  },
+  onReachBottom: function() {
+
+  },
+  onShareAppMessage: function() {
+
+  },
+  test:function(){
+    console.log(1)
+  }
+})
