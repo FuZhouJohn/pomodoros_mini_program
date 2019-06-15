c0b4f4d1cb2c877c1082fe68049c32bce31adb06
Zhuangjinan <a492073467@gmail.com>
2019-06-16 01:31:37 +0800

    绑定页面

---
+++ pages/binding/binding.js
@@ -0,0 +1,34 @@
+Page({
+  data: {
+    isBinding: true
+  },
+  switchStatus: function() {
+    this.setData({
+      isBinding: !this.data.isBinding
+    })
+  },
+  onLoad: function(options) {
+
+  },
+  onReady: function() {
+
+  },
+  onShow: function() {
+
+  },
+  onHide: function() {
+
+  },
+  onUnload: function() {
+
+  },
+  onPullDownRefresh: function() {
+
+  },
+  onReachBottom: function() {
+
+  },
+  onShareAppMessage: function() {
+
+  }
+})
