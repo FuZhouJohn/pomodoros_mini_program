d0d7dcea84219d5ee8d1f29048a5910abf9ec416
Zhuangjinan <a492073467@gmail.com>
2019-06-16 23:01:24 +0800

    一键登录功能

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,66 +1,53 @@
-// pages/login/login.js
+const {
+  http
+} = require('../../lib/http.js')
+const {
+  app_id,
+  app_secret
+} = getApp().globalData
+// url: /sign_in/mini_program_user
+// data: { code, iv, encrypted_data, app_id, app_secret }
+// method: post
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  login: function(e) {
+    let iv = e.detail.iv
+    let encrypted_data = e.detail.encryptedData
+    this.wxLogin(iv, encrypted_data)
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  wxLogin(iv, encrypted_data) {
+    wx.login({
+      success: res => {
+        if (res.code) {
+          this.signInUser(res.code, iv, encrypted_data)
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  signInUser(code, iv, encrypted_data) {
+    http.post('/sign_in/mini_program_user', {
+        code,
+        iv,
+        encrypted_data,
+        app_id,
+        app_secret
+      })
+      .then(response => {
+        this.saveInformation(response)
+        wx.reLaunch({
+          url: '/pages/home/home'
+        })
+      })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  saveInformation(response){
+    wx.setStorageSync('me', response.data.resource)
+    wx.setStorageSync("X-token", response.header['X-token'])
   }
 })
