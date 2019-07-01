afbb435d1d8e8282f2e4a68e8c5b23002d7ae128
Zhuangjinan <a492073467@gmail.com>
2019-07-01 23:22:46 +0800

    绑定页面接口接入

--- pages/binding/binding.js
+++ pages/binding/binding.js
@@ -1,14 +1,38 @@
+const {
+  http
+} = require('../../lib/http.js')
 Page({
   data: {
-    isBinding: true
-  },
-  switchStatus: function() {
-    this.setData({
-      isBinding: !this.data.isBinding
-    })
+    username: '',
+    password: ''
   },
   onLoad: function(options) {
 
+  },
+  usernameInput: function(e) {
+    this.data.username = e.detail.value
+  },
+  passwordInput: function(e) {
+    this.data.password = e.detail.value
+  },
+  bindingAccount() {
+    // if (this.data.username.trim() === '' || this.data.password.trim() === ''){
+    //   wx.showToast({
+    //     title: '用户名或密码不能为空',
+    //     icon:'none'
+    //   })
+    //   return 
+    // }
+    http.post('/bindings', {
+      account: this.data.username,
+      password_digest: this.data.password
+    })
+    .then(res=>{
+      wx.setStorageSync('me', res.data.resource)
+      wx.navigateBack({
+        
+      })
+    })
   },
   onReady: function() {
 
