abcf3035b20bf3b27b8dd4a3dba010ff989a837c
zhuangjinan <zhuangjinan2018@gmail.com>
2019-07-30 12:51:54 +0800

    自定义tabbar

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -1,35 +1,50 @@
 Component({
   data: {
-    selected: 1,
+    active: 0,
     color: "#7F8389",
     selectedColor: "#F6705A",
     list: [{
-      pagePath: "/pages/pomodoro/pomodoro",
-      iconPath: "/images/total.png",
-      selectedIconPath: "/images/total-active.png",
-      text: "专注"
-    }, {
       pagePath: "/pages/home/home",
       iconPath: "/images/home.png",
       selectedIconPath: "/images/home-active.png",
       text: "数据统计"
     }, {
+      pagePath: "/pages/pomodoro/pomodoro",
+      iconPath: "/images/total.png",
+      selectedIconPath: "/images/total-active.png",
+      text: "专注"
+    }, {
       pagePath: "/pages/me/me",
       iconPath: "/images/me.png",
       selectedIconPath: "/images/me-active.png",
       text: "我的"
-    }]
+    }],
+    show: true
   },
   attached() {},
   methods: {
-    switchTab(e) {
-      const data = e.currentTarget.dataset
-      const url = data.path
-      wx.switchTab({
-        url
-      })
+    onChange(event) {
       this.setData({
-        selected: data.index
+        active: event.detail
+      });
+      wx.switchTab({
+        url: this.data.list[event.detail].pagePath
+      });
+    },
+    init() {
+      const page = getCurrentPages().pop();
+      this.setData({
+        active: this.data.list.findIndex(item => item.pagePath === `/${page.route}`)
+      });
+    },
+    hideTabbar() {
+      this.setData({
+        show: false
+      })
+    },
+    showTabbar() {
+      this.setData({
+        show: true
       })
     }
   }
