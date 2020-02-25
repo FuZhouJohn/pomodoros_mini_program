074a33de05f594fac9eaebb194889f304d1d8590
庄骥南 <a492073467@gmail.com>
2020-02-26 00:41:48 +0800

    更新gitignore

---
+++ app.js
@@ -0,0 +1,12 @@
+App({
+  onLaunch: function () {
+
+  },
+  globalData: {
+    host: '服务端地址',
+    app_id: "小程序appid",
+    app_secret: "小程序appsecret",
+    t_app_id: "服务端appid",
+    t_app_secret: "服务端appsecret"
+  }
+})
